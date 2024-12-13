f76805442e1d12fc0e6bfd5327dc8eb963b35eb8
giujae <alacran6924@gmail.com>
2024-12-13 10:44:55 +0900

    build: add vite.config.ts settings for build
    - add @types/node, vite-tsconfig-paths
    - include tsconfingPaths() to plugins
    - include base options for preventing white pages when build
    - include build options to create seperate css, img, js folders

---
+++ vite.config.ts
@@ -0,0 +1,25 @@
+import { defineConfig } from 'vite'
+import react from '@vitejs/plugin-react'
+import tsconfigPaths from 'vite-tsconfig-paths'
+
+// https://vite.dev/config/
+export default defineConfig({
+  plugins: [react(), tsconfigPaths()],
+  base: process.env.NODE_ENV === 'development' ? '/' : './',
+  build: {
+    rollupOptions: {
+      output: {
+        assetFileNames: (assetInfo) => {
+          const name = assetInfo.name ?? ''
+          let extType = name.split('.').at(1) ?? 'unknown'
+          if (/png|jpe?g|svg|gif|tiff|bmp|ico/i.test(extType)) {
+            extType = 'img'
+          }
+          return `assets/${extType}/[name]-[hash][extname]`
+        },
+        chunkFileNames: 'assets/js/[name]-[hash].js',
+        entryFileNames: 'assets/js/[name]-[hash].js',
+      },
+    },
+  },
+})
