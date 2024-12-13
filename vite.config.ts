e889ac7c03ee1d82c6b00b23de788135b8ac0194
giujae <alacran6924@gmail.com>
2024-12-13 11:10:53 +0900

    build: add settings for fluid env use
    - fix base to env.VITE_APP_BASE_URL || './' for multiple env file use

--- vite.config.ts
+++ vite.config.ts
@@ -1,25 +1,34 @@
-import { defineConfig } from 'vite'
+import { defineConfig, loadEnv } from 'vite'
 import react from '@vitejs/plugin-react'
 import tsconfigPaths from 'vite-tsconfig-paths'
 
 // https://vite.dev/config/
-export default defineConfig({
-  plugins: [react(), tsconfigPaths()],
-  base: process.env.NODE_ENV === 'development' ? '/' : './',
-  build: {
-    rollupOptions: {
-      output: {
-        assetFileNames: (assetInfo) => {
-          const name = assetInfo.name ?? ''
-          let extType = name.split('.').at(1) ?? 'unknown'
-          if (/png|jpe?g|svg|gif|tiff|bmp|ico/i.test(extType)) {
-            extType = 'img'
-          }
-          return `assets/${extType}/[name]-[hash][extname]`
+export default defineConfig(({ mode }) => {
+  // Load environment variables based on the current mode
+  const env = loadEnv(mode, process.cwd(), '')
+
+  return {
+    plugins: [react(), tsconfigPaths()],
+    base: env.VITE_APP_BASE_URL || './',
+    build: {
+      rollupOptions: {
+        output: {
+          assetFileNames: (assetInfo) => {
+            const name = assetInfo.name ?? ''
+            let extType = name.split('.').at(1) ?? 'unknown'
+            if (/png|jpe?g|svg|gif|tiff|bmp|ico/i.test(extType)) {
+              extType = 'img'
+            }
+            return `assets/${extType}/[name]-[hash][extname]`
+          },
+          chunkFileNames: 'assets/js/[name]-[hash].js',
+          entryFileNames: 'assets/js/[name]-[hash].js',
         },
-        chunkFileNames: 'assets/js/[name]-[hash].js',
-        entryFileNames: 'assets/js/[name]-[hash].js',
       },
     },
-  },
+    // Define environment variables for the app
+    define: {
+      'process.env': env,
+    },
+  }
 })
