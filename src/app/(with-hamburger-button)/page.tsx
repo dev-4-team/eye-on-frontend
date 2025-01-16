858882c869262e8e6e382e5a632226d95aa08ec0
giujae <alacran6924@gmail.com>
2025-01-16 12:10:51 +0900

    feat: moved Home page to apply layout and added kakao map
    - Home page moved to with-hamburger-button folder
    - added Kakao Map Component as main content

---
+++ src/app/(with-hamburger-button)/page.tsx
@@ -0,0 +1,13 @@
+import KakaoMap from '@/components/KakaoMaps/KakaoMap';
+import ProtestInfos from '@/lib/API/ProtestInfos';
+
+export default async function Home() {
+    const protests = await ProtestInfos({ date: '123' });
+    const latitude = 37.539581447331;
+    const longitude = 127.00787604008;
+    return (
+        <div>
+            <KakaoMap latitude={latitude} longitude={longitude} w="100%" h="100vh" l={8} protests={protests} />
+        </div>
+    );
+}
