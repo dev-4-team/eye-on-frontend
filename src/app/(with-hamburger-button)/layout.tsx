1d48653c0d35dc11e1bfa87dd78f04e2c258c7db
giujae <alacran6924@gmail.com>
2025-01-16 12:13:06 +0900

    feat: layout for home page
    - layout contains hanburger button and protest list sheet
    - can be used for any page in need of upper feature

---
+++ src/app/(with-hamburger-button)/layout.tsx
@@ -0,0 +1,11 @@
+import ProtestList from '@/components/Protest/protest-list';
+import { ReactNode } from 'react';
+
+export default function Layout({ children }: { children: ReactNode }) {
+    return (
+        <div>
+            <ProtestList />
+            {children}
+        </div>
+    );
+}
