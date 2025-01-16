4070c8d3e48a1ddfafbb414c63fbcf416bf3525f
giujae <alacran6924@gmail.com>
2025-01-16 12:19:27 +0900

    feat: add ProtestCard component
    - each card shows abstract infos of individual protest

---
+++ src/components/Protest/protest-card.tsx
@@ -0,0 +1,18 @@
+import { ProtestData } from '@/types';
+import Link from 'next/link';
+import { MdArrowOutward } from 'react-icons/md';
+
+export default function ProtestCard({ id, title, description, location }: ProtestData) {
+    return (
+        <Link href={`/protest/${id}`}>
+            <div className="flex flex-col gap-0.5 text-start bg-background-white rounded-md p-3">
+                <div className="flex justify-between">
+                    <h1 className="text-amber-500 font-bold">{title}</h1>
+                    <MdArrowOutward />
+                </div>
+                <p className="text-[11px] text-zinc-400">{location}</p>
+                <p className="text-[13px] truncate">{description}</p>
+            </div>
+        </Link>
+    );
+}
