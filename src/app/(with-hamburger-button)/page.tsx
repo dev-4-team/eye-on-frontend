ee0d869bb13a7f4fe38a0123333edd723300491b
giujae <alacran6924@gmail.com>
2025-01-22 11:20:53 +0900

    fix: 100vh changed to calculated --vh

--- src/app/(with-hamburger-button)/page.tsx
+++ src/app/(with-hamburger-button)/page.tsx
@@ -7,7 +7,14 @@ export default async function Home() {
     const longitude = 127.00787604008;
     return (
         <div>
-            <KakaoMap latitude={latitude} longitude={longitude} w="100%" h="100vh" l={8} protests={protests} />
+            <KakaoMap
+                latitude={latitude}
+                longitude={longitude}
+                w="100%"
+                h="calc(var(--vh, 1vh) * 100)"
+                l={8}
+                protests={protests}
+            />
         </div>
     );
 }
