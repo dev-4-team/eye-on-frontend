c57b30870e0d48950fd10881b5554ecd94cee830
giujae <alacran6924@gmail.com>
2025-01-22 12:18:13 +0900

    chore: test logic to see if it works will be rolled back

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -11,6 +11,7 @@ export default function RootLayout({
     modal: ReactNode;
 }>) {
     const KAKAO_API_KEY = process.env.NEXT_PUBLIC_KAKAO_API_KEY;
+
     return (
         <html lang="kr">
             <body>
@@ -18,10 +19,24 @@ export default function RootLayout({
                     strategy="beforeInteractive"
                     src={`//dapi.kakao.com/v2/maps/sdk.js?appkey=${KAKAO_API_KEY}&libraries=services&autoload=false`}
                 ></Script>
-                <HeightSetter>
-                    <main>{children}</main>
-                </HeightSetter>
+                {/* <HeightSetter> */}
+                <main>{children}</main>
+                {/* </HeightSetter> */}
                 {modal}
+                <Script
+                    id="vh-fix"
+                    strategy="afterInteractive"
+                    dangerouslySetInnerHTML={{
+                        __html: `
+              function setVH() {
+                const vh = window.innerHeight * 0.01;
+                document.documentElement.style.setProperty('--vh', \`\${vh}px\`);
+              }
+              setVH();
+              window.addEventListener('resize', setVH);
+            `,
+                    }}
+                />
             </body>
         </html>
     );
