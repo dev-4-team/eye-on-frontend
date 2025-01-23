525f7e9e0faa7f350cf839cc91199780fdbd96dc
giujae <alacran6924@gmail.com>
2025-01-23 11:28:09 +0900

    feat: remove height setter
    - became unnessesary due to use of dvh

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -19,24 +19,8 @@ export default function RootLayout({
                     strategy="beforeInteractive"
                     src={`//dapi.kakao.com/v2/maps/sdk.js?appkey=${KAKAO_API_KEY}&libraries=services&autoload=false`}
                 ></Script>
-                {/* <HeightSetter> */}
                 <main>{children}</main>
-                {/* </HeightSetter> */}
                 {modal}
-                <Script
-                    id="vh-fix"
-                    strategy="afterInteractive"
-                    dangerouslySetInnerHTML={{
-                        __html: `
-              function setVH() {
-                const vh = window.innerHeight * 0.01;
-                document.documentElement.style.setProperty('--vh', \`\${vh}px\`);
-              }
-              setVH();
-              window.addEventListener('resize', setVH);
-            `,
-                    }}
-                />
             </body>
         </html>
     );
