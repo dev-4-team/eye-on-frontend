fef8e8b0e42fd93362af93c40df28b09242d5b35
giujae <alacran6924@gmail.com>
2025-01-23 11:24:06 +0900

    feat: remove height setter
    - from setting height from window.InnerHeight
    - to using dvh

--- src/components/HeightSetter.tsx
+++
@@ -1,9 +0,0 @@
-'use client';
-
-import useViewportHeightSetter from '@/hooks/useViewportHeightSetter';
-
-export default function HeightSetter({ children }: { children: React.ReactNode }) {
-    useViewportHeightSetter();
-
-    return <main>{children}</main>;
-}
