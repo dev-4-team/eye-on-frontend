8dc04f43c4a98453a67d591b0bf5339bfd4b4834
giujae <alacran6924@gmail.com>
2025-01-23 15:57:31 +0900

    feat: add temporal kakao login logic
    - send get http request and recieves accesstoken through header

---
+++ src/components/KakaoLogin/KakaoLogin.tsx
@@ -0,0 +1,31 @@
+'use client';
+
+export default function KakaoLogin() {
+    const onLoginClick = async () => {
+        const SERVER_URL = process.env.NEXT_PUBLIC_SERVER_DEV_URL;
+        const response = await fetch(`${SERVER_URL}/oauth2/authorization/kakao`);
+
+        if (!response.ok) {
+            console.error('오류:', response.status);
+            return <div>오류 발생</div>;
+        }
+
+        const authorizationHeader = response.headers.get('Authorization');
+
+        if (authorizationHeader && authorizationHeader.startsWith('Bearer ')) {
+            const accessToken = authorizationHeader.split(' ')[1];
+            console.log('Access Token: ', accessToken);
+            localStorage.setItem('accessToken', accessToken);
+        } else {
+            console.error('Access token not found in header');
+        }
+    };
+
+    return (
+        <div className="absolute bottom-0 right-0 z-50">
+            <button onClick={onLoginClick} className="p-5 bg-yellow-500">
+                login
+            </button>
+        </div>
+    );
+}
