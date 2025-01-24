445b108282c8d6ce928aebe65d8615b986a773e0
giujae <alacran6924@gmail.com>
2025-01-24 16:13:07 +0900

    feat: removed unused option
    - removed proxy option

--- next.config.ts
+++ next.config.ts
@@ -2,14 +2,6 @@ import type { NextConfig } from 'next';
 
 const nextConfig: NextConfig = {
     /* config options here */
-    async rewrites() {
-        return [
-            {
-                source: '/api/kakao',
-                destination: 'http://localhost:8080/oauth2/authrization/kakao',
-            },
-        ];
-    },
 };
 
 export default nextConfig;
