f6b0f06c03ef15f571e4cd62b6b85f8bd6216ae4
giujae <alacran6924@gmail.com>
2025-02-07 12:01:58 +0900

    feat: changed temporal params for getting protest data
    - from 2035-01-01 to 2024-03-15

--- src/app/protest/[id]/page.tsx
+++ src/app/protest/[id]/page.tsx
@@ -2,10 +2,11 @@ import StaticKakakoMap from '@/components/KakaoMaps/StaticKakaoMap';
 import ProtestDetailInfo from '@/components/Protest/protest-detail-info';
 import ProtestDetail from '@/lib/API/ProtestDetail';
 import { ProtestData } from '@/types';
+// import Verification from '@/components/Verification/Verification';
 
 export async function generateStaticParams() {
     const SERVER_URL = process.env.NEXT_PUBLIC_SERVER_DEV_URL;
-    const response = await fetch(`${SERVER_URL}/api/protest?date=2035-01-01`, { cache: 'force-cache' });
+    const response = await fetch(`${SERVER_URL}/api/protest?date=2025-03-15`, { cache: 'force-cache' });
 
     if (!response.ok) {
         throw new Error(response.statusText);
@@ -33,9 +34,14 @@ export default async function Page({ params }: { params: Promise<{ id: string }>
 
     return (
         <section className="bg-zinc-100 h-[100dvh] w-full min-w-[260px]">
-            <div className="flex flex-col gap-0.1 py-3 bg-background-white w-full">
-                <h1 className="text-amber-500 w-[85%] min-w-[240px] mx-auto font-bold">{title}</h1>
-                <span className="text-zinc-400 w-[85%] min-w-[240px] mx-auto  text-sm">{location}</span>
+            <div className="flex bg-background-white">
+                <div className="flex flex-col gap-0.1 py-3 bg-background-white w-[80%]">
+                    <h1 className="text-amber-500 w-[85%] min-w-[240px] mx-auto font-bold">{title}</h1>
+                    <span className="text-zinc-400 w-[85%] min-w-[240px] mx-auto  text-sm">{location}</span>
+                </div>
+                {/* <div className="w-[20%]">
+                    <Verification />
+                </div> */}
             </div>
             <div className="flex flex-col h-[calc(100%-68px)] justify-evenly">
                 <ProtestDetailInfo name={'시위 정보'} info={description} />
