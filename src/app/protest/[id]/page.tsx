cc80907f2f8e4b91ccfa2ec9672f49b17caff95a
pigpgw <ceh20002@naver.com>
2025-02-13 16:04:31 +0900

    refactor : refactor protest detail UI for apply feedback

--- src/app/protest/[id]/page.tsx
+++ src/app/protest/[id]/page.tsx
@@ -2,6 +2,7 @@ import StaticKakakoMap from '@/components/KakaoMaps/StaticKakaoMap';
 import ProtestDetailInfo from '@/components/Protest/protest-detail-info';
 import ProtestDetail from '@/lib/API/ProtestDetail';
 import { ProtestData } from '@/types';
+import { formatDate } from '@/lib/utils';
 // import Verification from '@/components/Verification/Verification';
 
 export async function generateStaticParams() {
@@ -29,26 +30,26 @@ export default async function Page({ params }: { params: Promise<{ id: string }>
     const { id, title, description, startDateTime, endDateTime, location, organizer, declaredParticipants, locations } =
         protest;
 
-    const startTime = new Date(startDateTime).toLocaleString();
-    const endTime = new Date(endDateTime).toLocaleString();
+    const startTime = formatDate(startDateTime);
+    const endTime = formatDate(endDateTime);
 
     return (
-        <section className="bg-zinc-100 h-[100dvh] w-full min-w-[260px]">
-            <div className="flex bg-background-white">
-                <div className="flex flex-col gap-0.1 py-3 bg-background-white w-[80%]">
-                    <h1 className="text-amber-500 w-[85%] min-w-[240px] mx-auto font-bold">{title}</h1>
-                    <span className="text-zinc-400 w-[85%] min-w-[240px] mx-auto  text-sm">{location}</span>
+        <section className='bg-zinc-100 h-[100dvh] w-full min-w-[260px]'>
+            <div className='flex bg-background-white'>
+                <div className='flex flex-col gap-0.1 py-3 bg-background-white w-[80%]'>
+                    <h1 className='text-amber-500 w-[85%] min-w-[240px] mx-auto font-bold'>{title}</h1>
+                    <span className='text-zinc-400 w-[85%] min-w-[240px] mx-auto  text-sm'>{location}</span>
                 </div>
                 {/* <div className="w-[20%]">
                     <Verification />
                 </div> */}
             </div>
-            <div className="flex flex-col h-[calc(100%-68px)] justify-evenly">
+            <div className='flex flex-col h-[calc(100%-68px)] justify-evenly'>
                 <ProtestDetailInfo name={'시위 정보'} info={description} />
                 <ProtestDetailInfo name={'시작 일시'} info={startTime} />
                 <ProtestDetailInfo name={'종료 일시'} info={endTime} />
                 <ProtestDetailInfo name={'주최자'} info={organizer} />
-                <ProtestDetailInfo name={'예상 참가 인원'} info={declaredParticipants.toString()} />
+                <ProtestDetailInfo name={'예상 참가 인원'} info={`${declaredParticipants.toLocaleString()}명`} />
                 <StaticKakakoMap
                     latitude={locations[0].latitude}
                     longitude={locations[0].longitude}
--- src/components/Protest/protest-list.tsx
+++ src/components/Protest/protest-list.tsx
@@ -3,21 +3,23 @@ import { GiHamburgerMenu } from 'react-icons/gi';
 import ProtestCard from './protest-card';
 import ProtestInfos from '@/lib/API/ProtestInfos';
 import { ProtestData } from '@/types';
+import { useEffect } from 'react';
 
 export default async function ProtestList() {
     const protests = await ProtestInfos({ date: '123' });
+    console.log(protests);
 
     return (
         <div>
             <Sheet>
-                <SheetTrigger className="absolute top-4 right-0 p-2 rounded-s-md bg-amber-500 text-2xl text-background-white z-10">
+                <SheetTrigger className='absolute top-4 right-0 p-2 rounded-s-md bg-amber-500 text-2xl text-background-white z-10'>
                     <GiHamburgerMenu />
                 </SheetTrigger>
-                <SheetContent className="w-[80%] p-3 bg-transparent border-transparent overflow-auto">
-                    <SheetHeader className="mt-11 gap-1 ">
-                        <SheetTitle className="hidden">ProtestList</SheetTitle>
-                        <SheetDescription className="hidden">ProtestList</SheetDescription>
-                        {protests.map((protest: ProtestData, idx: number) => (
+                <SheetContent className='w-[80%] p-3 bg-transparent border-transparent overflow-auto'>
+                    <SheetHeader className='mt-11 gap-1 '>
+                        <SheetTitle className='hidden'>ProtestList</SheetTitle>
+                        <SheetDescription className='hidden'>ProtestList</SheetDescription>
+                        {protests?.map((protest: ProtestData, idx: number) => (
                             <ProtestCard key={idx} {...protest} />
                         ))}
                     </SheetHeader>
