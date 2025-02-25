c5bfe9f365cfe53409719b56aa42f3accd3b7996
giujae <alacran6924@gmail.com>
2025-02-25 12:20:02 +0900

    refactor: added keywords for google seo
    - google no longer uses meta tag keyword
    - desired keywords should be added to main contents

--- src/components/Footer/index.tsx
+++ src/components/Footer/index.tsx
@@ -4,12 +4,13 @@ import styles from './Footer.module.css';
 export default function Footer() {
     return (
         <footer className={styles.footer}>
+            <h2>오늘의 집회 및 시위 일정을 확인해보세요!</h2>
             <p>
                 건의사항이 있으신가요?&nbsp;
                 <Link
-                    href='https://forms.gle/yVCSNEFvB6rA95QW7'
-                    target='_blank'
-                    rel='noopener noreferrer'
+                    href="https://forms.gle/yVCSNEFvB6rA95QW7"
+                    target="_blank"
+                    rel="noopener noreferrer"
                     className={styles.link}
                 >
                     Google 설문 링크
@@ -17,7 +18,7 @@ export default function Footer() {
             </p>
             <p>
                 문의 :&nbsp;
-                <a href='mailto:eye.on.korea25@google.com' className={styles.email}>
+                <a href="mailto:eye.on.korea25@google.com" className={styles.email}>
                     eye.on.korea25@google.com
                 </a>
             </p>
