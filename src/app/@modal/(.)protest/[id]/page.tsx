fdafafd38434f0c20adde629060c304a85630ce3
pigpgw <ceh20002@naver.com>
2025-02-27 21:11:35 +0900

    fix : fix bug

--- src/app/@modal/(.)protest/[id]/page.tsx
+++ src/app/@modal/(.)protest/[id]/page.tsx
@@ -2,6 +2,7 @@
 import ProtestPage from '@/app/protest/[id]/page';
 import ModalWrapper from '@/components/Modal/ModalWrapper';
 
+// eslint-disable-next-line @typescript-eslint/no-explicit-any
 export default function Page(props: any) {
     return (
         <ModalWrapper>
