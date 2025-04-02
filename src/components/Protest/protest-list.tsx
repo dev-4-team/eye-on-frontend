b4d36be366cc27decdf5237e29dc7da311342ed1
pigpgw <ceh20002@naver.com>
2025-04-02 13:12:30 +0900

    refactor : protest list sidebar closed when user clicked empty space

--- src/components/Protest/protest-list.tsx
+++ src/components/Protest/protest-list.tsx
@@ -8,21 +8,19 @@ export default async function ProtestList() {
     const date = new Date().toISOString().split('T')[0];
     const protests = await ProtestInfos({ date: date });
     return (
-        <div>
-            <Sheet>
-                <SheetTrigger className='absolute top-12 right-0 p-2 rounded-s-md bg-[#D44646] text-2xl text-background-white z-10'>
-                    <GiHamburgerMenu />
-                </SheetTrigger>
-                <SheetContent className='w-[80%] p-3 bg-transparent border-transparent overflow-auto '>
-                    <SheetHeader className='mt-11 gap-1 '>
-                        <SheetTitle className='hidden'>ProtestList</SheetTitle>
-                        <SheetDescription className='hidden'>ProtestList</SheetDescription>
-                        {protests?.map((protest: ProtestData, idx: number) => (
-                            <ProtestCard key={idx} {...protest} />
-                        ))}
-                    </SheetHeader>
-                </SheetContent>
-            </Sheet>
-        </div>
+        <Sheet>
+            <SheetTrigger className='absolute top-12 right-0 p-2 rounded-s-md bg-[#D44646] text-2xl text-background-white z-10'>
+                <GiHamburgerMenu />
+            </SheetTrigger>
+            <SheetContent className='w-[80%] h-5 p-3 bg-transparent border-transparent '>
+                <SheetHeader className='mt-11 gap-1 overflow-hidden'>
+                    <SheetTitle className='hidden'>ProtestList</SheetTitle>
+                    <SheetDescription className='hidden'>ProtestList</SheetDescription>
+                    {protests?.map((protest: ProtestData, idx: number) => (
+                        <ProtestCard key={idx} {...protest} />
+                    ))}
+                </SheetHeader>
+            </SheetContent>
+        </Sheet>
     );
 }
