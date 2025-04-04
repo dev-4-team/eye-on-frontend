bbe7c9e57fe0fd8aed8cdd27809c2bcd0b62e74e
giujae <alacran6924@gmail.com>
2025-04-04 15:22:37 +0900

    test: images backup and test

---
+++ src/app/verified/page.tsx
@@ -0,0 +1,11 @@
+import Image from 'next/image';
+
+export default function Page() {
+    return (
+        <div className="flex justify-center h-[86vh]">
+            <div>
+                <Image src={'/images/check.gif'} width={100} height={100} alt="check" />
+            </div>
+        </div>
+    );
+}
