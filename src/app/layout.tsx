e3c021f89207a8dff69aeb5c4e38ed20cb58433e
giujae <alacran6924@gmail.com>
2025-04-08 12:54:17 +0900

    feat: added toaster optioins
    - theme set to system default
    - richColors options for colors
    - set duration of toaster to 2000ms

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -10,44 +10,44 @@ import TanStackProvider from '@/app/providers/TanStackProvider';
 import { SocketProvider } from '@/app/providers/SocketProvider';
 
 export const metadata: Metadata = {
-    verification: {
-        google: 'C7IMZBZEv09U7b7X4-rcEn7Fe1hpK5yE0EfGgNgxRPE',
-    },
-    other: {
-        'naver-site-verification': 'eef43fcd0d4ac4541e99553df2570bfa06bdc56a',
-    },
+  verification: {
+    google: 'C7IMZBZEv09U7b7X4-rcEn7Fe1hpK5yE0EfGgNgxRPE',
+  },
+  other: {
+    'naver-site-verification': 'eef43fcd0d4ac4541e99553df2570bfa06bdc56a',
+  },
 };
 
 export default function RootLayout({
-    children,
-    modal,
+  children,
+  modal,
 }: Readonly<{
-    children: ReactNode;
-    modal: ReactNode;
+  children: ReactNode;
+  modal: ReactNode;
 }>) {
-    const KAKAO_API_KEY = process.env.NEXT_PUBLIC_KAKAO_API_KEY;
+  const KAKAO_API_KEY = process.env.NEXT_PUBLIC_KAKAO_API_KEY;
 
-    return (
-        <html lang='kr'>
-            <body>
-                {process.env.NEXT_PUBLIC_GOOGLE_ANALYTICS && (
-                    <GoogleAnalytics gaId={process.env.NEXT_PUBLIC_GOOGLE_ANALYTICS} />
-                )}
-                <Header />
-                <Script
-                    strategy='lazyOnload'
-                    src={`//dapi.kakao.com/v2/maps/sdk.js?appkey=${KAKAO_API_KEY}&libraries=services&autoload=false`}
-                />
-                <Script strategy='lazyOnload' src='https://unpkg.com/heatmap.js' />
-                <TanStackProvider>
-                    <SocketProvider>
-                        <main>{children}</main>
-                        {modal}
-                        <Toaster />
-                    </SocketProvider>
-                </TanStackProvider>
-                <Footer />
-            </body>
-        </html>
-    );
+  return (
+    <html lang='kr'>
+      <body>
+        {process.env.NEXT_PUBLIC_GOOGLE_ANALYTICS && (
+          <GoogleAnalytics gaId={process.env.NEXT_PUBLIC_GOOGLE_ANALYTICS} />
+        )}
+        <Header />
+        <Script
+          strategy='lazyOnload'
+          src={`//dapi.kakao.com/v2/maps/sdk.js?appkey=${KAKAO_API_KEY}&libraries=services&autoload=false`}
+        />
+        <Script strategy='lazyOnload' src='https://unpkg.com/heatmap.js' />
+        <TanStackProvider>
+          <SocketProvider>
+            <main>{children}</main>
+            {modal}
+            <Toaster theme='system' richColors duration={2000} />
+          </SocketProvider>
+        </TanStackProvider>
+        <Footer />
+      </body>
+    </html>
+  );
 }
