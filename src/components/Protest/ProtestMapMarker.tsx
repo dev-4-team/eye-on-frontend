06ce045ac72a760cf3e0ff6ea780b4baf819625c
pigpgw <ceh20002@naver.com>
2025-04-08 16:46:28 +0900

    feat: zoom to level 4 on marker click

--- src/components/Protest/ProtestMapMarker.tsx
+++ src/components/Protest/ProtestMapMarker.tsx
@@ -8,59 +8,65 @@ import { UseProtestCheerCount } from '@/hooks/UseProtestCheerCount';
 import { numberTransfer } from '@/lib/utils';
 
 export default function ProtestMapMarker({
-    protest,
-    mapInstance,
-    router,
+  protest,
+  mapInstance,
+  router,
 }: {
-    protest: ProtestData;
-    mapInstance: any;
-    router: AppRouterInstance;
+  protest: ProtestData;
+  mapInstance: any;
+  router: AppRouterInstance;
 }) {
-    const [verifiedNumber, setVerifiedNumber] = useState(0);
+  const [verifiedNumber, setVerifiedNumber] = useState(0);
 
-    useEffect(() => {
-        const verifyNumber = async () => {
-            const result = await getVerificationNumber(protest.id);
-            setVerifiedNumber(result.verifiedNum);
-        };
-        verifyNumber();
-    }, [protest]);
-
-    const maxVerified = 5000;
-    const baseZIndex = 10;
-    const maxZIndex = 100;
-    const dynamicZIndex = Math.min(maxZIndex, baseZIndex + (verifiedNumber / maxVerified) * maxZIndex);
-
-    const onMarkerClick = (id: string, lat: number, long: number) => {
-        if (mapInstance) {
-            const destLatLng = new kakao.maps.LatLng(lat, long);
-            mapInstance.panTo(destLatLng);
-        }
-        router.push(`/protest/${id}`);
+  useEffect(() => {
+    const verifyNumber = async () => {
+      const result = await getVerificationNumber(protest.id);
+      setVerifiedNumber(result.verifiedNum);
     };
-    const { data } = UseProtestCheerCount(protest.id);
-    const { effect } = useCheerEffect(data);
+    verifyNumber();
+  }, [protest]);
 
-    return (
-        <div>
-            <CustomOverlayMap
-                position={{
-                    lat: protest.locations[0].latitude,
-                    lng: protest.locations[0].longitude,
-                }}
-                yAnchor={1}
-                zIndex={dynamicZIndex}
-            >
-                <div
-                    className=' p-5  bg-[url(/images/marker.png)] bg-center bg-no-repeat bg-contain  cursor-pointer  flex flex-col items-center justify-center'
-                    onClick={() =>
-                        onMarkerClick(protest.id, protest.locations[0].latitude, protest.locations[0].longitude)
-                    }
-                >
-                    {effect && <div className='absolute bottom-10'>🔥</div>}
-                    <span className='text-xs pb-2 font-sans font-bold'>{numberTransfer(data?.cheerCount || 0)}</span>
-                </div>
-            </CustomOverlayMap>
+  const maxVerified = 5000;
+  const baseZIndex = 10;
+  const maxZIndex = 100;
+  const dynamicZIndex = Math.min(
+    maxZIndex,
+    baseZIndex + (verifiedNumber / maxVerified) * maxZIndex,
+  );
+
+  const onMarkerClick = (id: string, lat: number, long: number) => {
+    if (mapInstance) {
+      mapInstance.setLevel(4);
+      const destLatLng = new kakao.maps.LatLng(lat, long);
+      mapInstance.panTo(destLatLng);
+    }
+    router.push(`/protest/${id}`);
+  };
+  const { data } = UseProtestCheerCount(protest.id);
+  const { effect } = useCheerEffect(data);
+
+  return (
+    <div>
+      <CustomOverlayMap
+        position={{
+          lat: protest.locations[0].latitude,
+          lng: protest.locations[0].longitude,
+        }}
+        yAnchor={1}
+        zIndex={dynamicZIndex}
+      >
+        <div
+          className=' p-5  bg-[url(/images/marker.png)] bg-center bg-no-repeat bg-contain  cursor-pointer  flex flex-col items-center justify-center'
+          onClick={() =>
+            onMarkerClick(protest.id, protest.locations[0].latitude, protest.locations[0].longitude)
+          }
+        >
+          {effect && <div className='absolute bottom-10'>🔥</div>}
+          <span className='text-xs pb-2 font-sans font-bold'>
+            {numberTransfer(data?.cheerCount || 0)}
+          </span>
         </div>
-    );
+      </CustomOverlayMap>
+    </div>
+  );
 }
