ef436de49bdf71eb73d3d2e42d7d617473e310a0
pigpgw <ceh20002@naver.com>
2025-04-09 23:10:00 +0900

    feat/#74 : 카카오 맵 로딩 컴포넌트와 에러 컴포넌트를 추가 및 불필요한 의존성을 삭제한다

---
+++ src/components/KakaoMaps/MapLoadingFallback.tsx
@@ -0,0 +1,12 @@
+export const MapLoadingFallback = () => {
+  return (
+    <div className='flex h-[calc(100dvh-14vh)] items-center justify-center relative overflow-hidden bg-gray-100 rounded-lg'>
+      <div className='relative z-10 bg-white rounded-full p-5 shadow-xl'>
+        <div className='w-16 h-16 border-4 border-red-400 border-t-transparent rounded-full animate-spin'></div>
+      </div>
+      <div className='absolute bottom-8 left-0 right-0 text-center text-gray-600 font-medium'>
+        지도 데이터를 불러오는 중입니다...
+      </div>
+    </div>
+  );
+};
