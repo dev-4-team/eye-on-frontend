871c0b78d291b4aeff692a545d0c744024e0df58
giujae <alacran6924@gmail.com>
2025-04-18 13:08:23 +0900

    feature/#82 : 인증완료 페이지의 gif를 설정하고, 인증 완료 후 이전 페이지로 돌아가기 버튼을 만든다

--- src/app/verified/page.tsx
+++ src/app/verified/page.tsx
@@ -1,10 +1,24 @@
+'use client';
+
+import { Button } from '@/components/ui/button';
 import Image from 'next/image';
+import { useRouter } from 'next/navigation';
 
 export default function Page() {
+  const router = useRouter();
+  const handleGobackButton = () => {
+    router.back();
+  };
   return (
-    <div className='flex justify-center h-[86vh]'>
-      <div>
-        <Image src={'/images/check.gif'} width={100} height={100} alt='check' />
+    <div className='flex justify-center items-center h-[86vh]'>
+      <div className='flex flex-col h-[100%] items-center justify-around'>
+        <div className='flex flex-col items-center gap-20'>
+          <span className='text-xl font-bold'>인증이 완료되었습니다.</span>
+          <Image src={'/images/check.gif'} width={200} height={200} alt='check' />
+        </div>
+        <Button onClick={handleGobackButton} variant={'signature'} size={'verify'}>
+          인증완료
+        </Button>
       </div>
     </div>
   );
