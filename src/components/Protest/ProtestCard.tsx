d03f680fbbfd0254f95f3305ee599b47e93e25dc
pigpgw <ceh20002@naver.com>
2025-04-19 23:56:21 +0900

    refactor/#94 : 서버 api response 변경사항으로 인한 시위 제목을 추가한다

--- src/components/Protest/ProtestCard.tsx
+++ src/components/Protest/ProtestCard.tsx
@@ -2,7 +2,7 @@ import Link from 'next/link';
 import { MdArrowOutward } from 'react-icons/md';
 import { Protest } from '@/types/protest';
 
-const ProtestCard = ({ id, title, description }: Protest) => {
+const ProtestCard = ({ id, title, description, locations }: Protest) => {
   return (
     <Link href={`/protest/${id}`}>
       <div className='flex flex-col gap-0.5 text-start bg-background-white rounded-md p-3'>
@@ -10,7 +10,7 @@ const ProtestCard = ({ id, title, description }: Protest) => {
           <h1 className='text-[#D44646] font-bold'>{title}</h1>
           <MdArrowOutward />
         </div>
-        {/* <p className='text-[11px] text-zinc-400'>{location}</p> */}
+        <p className='text-[11px] text-zinc-400'>{locations[0].name}</p>
         <p className='text-[13px] truncate'>{description}</p>
       </div>
     </Link>
