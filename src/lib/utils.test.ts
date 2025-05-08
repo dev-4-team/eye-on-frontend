b43542ae569e456cf34cb1a68c34c3a82945d740
pigpgw <ceh20002@naver.com>
2025-05-08 10:46:46 +0900

    feature/test-#129 : formatDate의 반환값이 요일이 올바른지 테스트하는 코드를 추가한다
    - toMatch를 사용한 테스트를 진행하였습니다.

--- src/lib/utils.test.ts
+++ src/lib/utils.test.ts
@@ -1,6 +1,6 @@
 import { formatDate } from '@/lib/utils';
 
-describe('testing formatDate', () => {
+describe('testing formatDate toEqual', () => {
   test('correctly return monday', () => {
     const testDate = new Date(2025, 5, 2).toISOString();
     expect(formatDate(testDate)).toEqual('6월 2일 월요일 0시 0분');
@@ -28,5 +28,36 @@ describe('testing formatDate', () => {
   test('correctly return sunday', () => {
     const testDate = new Date(2025, 5, 8).toISOString();
     expect(formatDate(testDate)).toEqual('6월 8일 일요일 0시 0분');
+  });
+});
+
+describe('testing formatDate correctly contain day of week', () => {
+  test('return correctly contain monday', () => {
+    const testDate = new Date(2025, 5, 2).toISOString();
+    expect(formatDate(testDate)).toMatch('월요일');
+  });
+  test('return correctly contain tuesday', () => {
+    const testDate = new Date(2025, 5, 3).toISOString();
+    expect(formatDate(testDate)).toMatch('화요일');
+  });
+  test('return correctly contain wednesday', () => {
+    const testDate = new Date(2025, 5, 4).toISOString();
+    expect(formatDate(testDate)).toMatch('수요일');
+  });
+  test('return correctly contain thursday', () => {
+    const testDate = new Date(2025, 5, 5).toISOString();
+    expect(formatDate(testDate)).toMatch('목요일');
+  });
+  test('return correctly contain friday', () => {
+    const testDate = new Date(2025, 5, 6).toISOString();
+    expect(formatDate(testDate)).toMatch('금요일');
+  });
+  test('return correctly contain saturday', () => {
+    const testDate = new Date(2025, 5, 7).toISOString();
+    expect(formatDate(testDate)).toMatch('토요일');
+  });
+  test('return correctly contain sunday', () => {
+    const testDate = new Date(2025, 5, 8).toISOString();
+    expect(formatDate(testDate)).toMatch('일요일');
   });
 });
