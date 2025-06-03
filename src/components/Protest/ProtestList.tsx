cf20d5c519054c74cea5709d963bf7afc3b56a33
pigpgw <ceh20002@naver.com>
2025-06-03 16:45:11 +0900

    refactor/#141 : 접근성으 ㄹ위한 의미있는 제목과 설명을 제공한다

--- src/components/Protest/ProtestList.tsx
+++ src/components/Protest/ProtestList.tsx
@@ -5,14 +5,14 @@ import {
   SheetHeader,
   SheetTitle,
   SheetTrigger,
-} from '@/components/ui/sheet';
-import { GiHamburgerMenu } from 'react-icons/gi';
-import ProtestCard from '@/components/Protest/ProtestCard';
-import { getProtestList } from '@/api/protest';
-import type { Protest } from '@/types/protest';
+} from '@/components/ui/sheet'
+import { GiHamburgerMenu } from 'react-icons/gi'
+import ProtestCard from '@/components/Protest/ProtestCard'
+import { getProtestList } from '@/api/protest'
+import type { Protest } from '@/types/protest'
 
 const ProtestList = async () => {
-  const protests = await getProtestList();
+  const protests = await getProtestList()
   return (
     <Sheet>
       <SheetTrigger className='fixed top-24 sm:top-16 md:top-32 lg:top-64 right-0 p-2 rounded-s-md bg-[#D44646] text-2xl text-background-white z-10'>
@@ -20,13 +20,13 @@ const ProtestList = async () => {
       </SheetTrigger>
       <SheetContent className='w-[80%] p-3 overflow-y-auto'>
         <SheetHeader className='gap-1'>
-          <SheetTitle className='hidden'>ProtestList</SheetTitle>
-          <SheetDescription className='hidden'>ProtestList</SheetDescription>
+          <SheetTitle className='hidden'>시위 목록</SheetTitle>
+          <SheetDescription className='hidden'>진행중인 시위들을 확인하세요</SheetDescription>
           {protests?.map((protest: Protest) => <ProtestCard key={protest.id} {...protest} />)}
         </SheetHeader>
       </SheetContent>
     </Sheet>
-  );
-};
+  )
+}
 
-export default ProtestList;
+export default ProtestList
