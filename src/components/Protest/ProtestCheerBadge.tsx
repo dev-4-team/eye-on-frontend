ac32b2710ee8ec2db16bcbe50a2c1700db5572cd
pigpgw <ceh20002@naver.com>
2025-06-03 15:48:52 +0900

    refactor/#141 : ProtestCheerBadge 로직을 강화한다

--- src/components/Protest/ProtestCheerBadge.tsx
+++ src/components/Protest/ProtestCheerBadge.tsx
@@ -1,16 +1,16 @@
-'use client';
-import { EMOJI } from '@/constants/emojis';
-import { useCheerEffect } from '@/hooks/useCheerEffect';
-import { useProtestCheerCount } from '@/hooks/useProtestCheerCount';
+'use client'
+import { EMOJI } from '@/constants/emojis'
+import { useCheerEffect } from '@/hooks/useCheerEffect'
+import { useProtestCheerCount } from '@/hooks/useProtestCheerCount'
 interface Props {
-  protestId: string;
+  protestId: string
 }
 const ProtestCheerBadge = ({ protestId }: Props) => {
   const { cheerCount, cheerCountIsLoading, cheerCountIsError } = useProtestCheerCount({
     protestId,
-  });
-  const { effect } = useCheerEffect(cheerCount);
-  if (!protestId || cheerCountIsError || cheerCountIsLoading) return null;
+  })
+  const { effect } = useCheerEffect({ protestId, cheerCount: cheerCount?.cheerCount ?? 0 })
+  if (!protestId || cheerCountIsError || cheerCountIsLoading) return null
 
   return (
     <div className='flex flex-col justify-center items-center'>
@@ -23,7 +23,7 @@ const ProtestCheerBadge = ({ protestId }: Props) => {
         <div>{cheerCount?.cheerCount}</div>
       </>
     </div>
-  );
-};
+  )
+}
 
-export default ProtestCheerBadge;
+export default ProtestCheerBadge
